e7f71f42e8a4df3560c8d440115478ea5b60132b
anticomm <93606441+anticomm@users.noreply.github.com>
2025-09-19 02:00:15 +0300

    Update amazon_cep.py

--- amazon_cep.py
+++ amazon_cep.py
@@ -11,7 +11,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from telegram_cep import send_message
 
-URL = "https://www.amazon.com.tr/s?i=electronics&rh=n%3A12466496031%2Cn%3A13709880031%2Cn%3A13709907031%2Cp_123%3A46655%2Cp_98%3A21345978031%2Cp_6%3AA1UNQM1SR2CHM%257CA215JX4S9CANSO&dc&ds=v1%3A6iwARYk%2BRRi8kEeaA7TzLD9L7Vjp9PprtvbRXt8zH%2Bo"
+URL = "https://www.amazon.com.tr/s?i=electronics&rh=n%3A12466496031%2Cn%3A13709883031%2Cn%3A13709938031%2Cn%3A13710137031%2Cp_36%3A-5000000%2Cp_123%3A359121%2Cp_n_g-101013615904111%3A68100078031&dc&ds=v1%3A3hudIubMzEomyiDTLhL1252C8P8hj57vBestCoSe7Gg"
 COOKIE_FILE = "cookie_cep.json"
 SENT_FILE = "send_products.txt"
 
