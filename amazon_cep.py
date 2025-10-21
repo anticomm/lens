95e5a7501ba5b16e93520e8cbd3aa491fa53f3bf
anticomm <93606441+anticomm@users.noreply.github.com>
2025-10-21 17:58:23 +0300

    Remove unused subprocess import from amazon_cep.py

--- amazon_cep.py
+++ amazon_cep.py
@@ -5,7 +5,6 @@ import json
 import time
 import base64
 import re
-import subprocess
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
