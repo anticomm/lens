a271aa5c2a755dde6f2edcb148698ed2e3026bae
anticomm <93606441+anticomm@users.noreply.github.com>
2025-11-07 20:21:59 +0300

    Update telegram_cep.py

--- telegram_cep.py
+++ telegram_cep.py
@@ -61,8 +61,9 @@ def send_message(product):
     message = format_product_message(product)
     image_url = product.get("image")
     asin = product.get("asin")
-    link = f"https://indir.im/{asin}" if asin else product.get("link", "#")
-
+    real_link = f"https://indirimsinyali.com/Elektronik/{asin}.html" if asin else product.get("link", "#")
+    short_link = f"https://indir.im/{asin}" if asin else real_link
+   
     try:
         reply_markup = json.dumps({
             "inline_keyboard": [[
